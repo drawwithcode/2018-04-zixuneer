796bb872500a02ae40e2d47f93d75fbcf4612b15
Huang Zixun <13051190353@163.com>
2018-10-16 23:04:15 +0200

    object&array

--- sketch.js
+++ sketch.js
@@ -1,60 +1,91 @@
+
+var mySound;
+function preload() {
+
+   mySound = loadSound('./assets/thunder.mp3');
+  }
+
+
 var balls = [];
+var gravity = 0.1;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-
+   mySound.setVolume(0.5);
+   mySound.play();
 }
 
 function draw() {
-  frameRate(60);
-  translate(width/2,height/2);
 
-  background(0);
+  var col1=lerpColor(color(133, 193, 233),color(40, 55, 71),frameCount/200);
+  background(col1);
 
-  var s='move your mouse';
+  if(frameCount>200){
+
+  var s1='It is going to rain';
+  var s2='Create the raindrop by clicking';
   textFont('Aqua Grotesque');
+  textSize(10);
+  textStyle(NORMAL);
+  text(s2,70,200,200,200);
   textStyle(BOLD);
-  textSize(16);
-  fill(50, 20, 218);
-  text(s,-600,-270,500,500);
-
-  for(i=0; i<47; i++){
-    var x=mouseX/13;
-    var y=mouseY/13;
-    var r=i+3;
-
-    balls[i] = new Ball(x+i*3,y+i*3,r);
-  }
-
-
-  for (var j = 0; j < balls.length; j++) {
-    balls[j].move();
-    balls[j].display();
-  }
+  textSize(30);
+  fill(136, 78, 160);
+  text(s1,70,100,200,200);
 }
 
-function Ball(tempX, tempY, tempDiameter) {
+  var col2=lerpColor(color(133, 193, 233),color(133, 193, 233,50),frameCount/200);
+  noStroke();
+  fill(col2);
+  rect(0,height*0.8,width,height*0.8);
 
-  this.x = tempX;
-  this.y = tempY;
-  this.diameter = tempDiameter;
-  this.speed = 1;
+ for(var j=0; j<width; j+=128){
+    var x1=100;
+    var x2=40;
+    var y1=height;
+    var dis=100
+    stroke(200);
+    strokeWeight(5);
+    bezier(x1+j, y1*0.87, x2+j, y1*0.82, x1+5+j, y1*0.9, x2+5+j, y1*0.85);
+    bezier(x1+j+dis, y1*0.95, x2+j+dis, y1*0.9, x1+5+j+dis, y1*0.98, x2+5+j+dis, y1*0.93);
+}
 
-  this.move = function() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  };
+  for (var i = 0; i < balls.length; i ++ ) {
+    balls[i].move();
+    balls[i].display();
+  }
 
-  this.display = function() {
-    var col=lerpColor(color(222, 229, 22, 200),color(98, 20, 218, 200),frameCount/600);
-    if(frameCount>700){
-      col=color(random(50,98), random(0,50), random(200,220), random(130,255));
+}
+
+function mousePressed() {
+
+  var b = new Ball(mouseX,mouseY,18);
+  balls.push(b);
+}
+
+
+function Ball(tempX, tempY, tempW){
+    this.x = tempX;
+    this.y = tempY;
+    this.w = tempW;
+    this.speed = 0;
+
+    this.display=function(){
+      fill(215, 189, 226);
+      strokeWeight(1);
+      stroke(200);
+      ellipse(this.x,this.y,this.w);
     }
-    fill(col);
-    angleMode(RADIANS);
-    rotate(frameCount/350);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-    //text('x',0,0,100);
-  };
 
+    this.move=function(){
+
+    this.y = this.y + this.speed;
+    this.speed = this.speed + gravity;
+    if (this.y > height*0.8-10) {
+      this.y = height*0.8-10;
+      this.speed = this.speed * -0.97;
+  }
+
+
+ }
 }
